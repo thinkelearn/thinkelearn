39fdf96d1b1c86d2b94d735d938e9e9699e5050b
dr-rompecabezas <felavid@gmail.com>
2025-06-26 15:40:43 -0400

    Update email addresses for contact form and SMTP configuration

--- thinkelearn/settings/production.py
+++ thinkelearn/settings/production.py
@@ -116,7 +116,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-mail.outlook.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'hello@thinkelearn.com')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'f.villegas@thinkelearn.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'hello@thinkelearn.com')
 
