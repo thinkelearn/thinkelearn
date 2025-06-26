f23188658a5450ed87123cdd44cb4bf7131cd778
dr-rompecabezas <felavid@gmail.com>
2025-06-26 13:07:01 -0400

    Update email configuration to use Outlook SMTP settings

--- thinkelearn/settings/production.py
+++ thinkelearn/settings/production.py
@@ -102,11 +102,11 @@ CSRF_COOKIE_SECURE = True
 
 # Email configuration (for contact forms and admin notifications)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-mail.outlook.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'hello@thinkelearn.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'hello@thinkelearn.com')
 
 # Wagtail settings for production
